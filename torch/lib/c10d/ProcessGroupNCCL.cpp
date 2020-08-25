1df1f351933efaff23fae28d3a1f8e0d6f309b0a
Omkar Salpekar <osalpekar@fb.com>
2020-08-25 12:18:20 -0700

    [NCCL] Adding CUDA Stream Overwrite Options for Allgather, Reduce, ReduceScatter, and Broadcast
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/41276
    
    This uses the helper functions implemented in the previous PR in this stack to enable enqueuing operations on a user-provided CUDA stream for Allgather, Reduce, ReduceScatter, and Broadcast.
    ghstack-source-id: 110660517
    
    Differential Revision: [D22300227](https://our.internmc.facebook.com/intern/diff/D22300227/)

--- torch/lib/c10d/ProcessGroupNCCL.cpp
+++ torch/lib/c10d/ProcessGroupNCCL.cpp
@@ -935,9 +935,13 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::broadcast(
     const BroadcastOptions& opts) {
   check_gpu_tensors(tensors);
 
+  const NCCLBroadcastOptions& ncclOpts =
+      static_cast<const NCCLBroadcastOptions&>(opts);
+
   return collective(
       tensors,
       tensors,
+      ncclOpts.cudaStreams,
       [&](at::Tensor& input,
           at::Tensor& output,
           ncclComm_t comm,
@@ -958,9 +962,13 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::reduce(
     const ReduceOptions& opts) {
   check_gpu_tensors(tensors);
 
+  const NCCLReduceOptions& ncclOpts =
+      static_cast<const NCCLReduceOptions&>(opts);
+
   return collective(
       tensors,
       tensors,
+      ncclOpts.cudaStreams,
       [&](at::Tensor& input,
           at::Tensor& output,
           ncclComm_t comm,
@@ -984,6 +992,9 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::allgather(
     const AllgatherOptions& opts) {
   check_gpu_tensors(inputTensors);
 
+  const NCCLAllgatherOptions& ncclOpts =
+      static_cast<const NCCLAllgatherOptions&>(opts);
+
   auto outputFlattened =
       flatten_for_scatter_gather(outputTensors, inputTensors, size_);
   check_gpu_tensors(outputFlattened);
@@ -991,6 +1002,7 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::allgather(
   return collective(
       inputTensors,
       outputFlattened,
+      ncclOpts.cudaStreams,
       [&](at::Tensor& input,
           at::Tensor& output,
           ncclComm_t comm,
@@ -1035,6 +1047,9 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::reduce_scatter(
     const ReduceScatterOptions& opts) {
   check_gpu_tensors(outputTensors);
 
+  const NCCLReduceScatterOptions& ncclOpts =
+      static_cast<const NCCLReduceScatterOptions&>(opts);
+
   auto inputFlattened =
       flatten_for_scatter_gather(inputTensors, outputTensors, size_);
   check_gpu_tensors(inputFlattened);
@@ -1042,6 +1057,7 @@ std::shared_ptr<ProcessGroup::Work> ProcessGroupNCCL::reduce_scatter(
   return collective(
       inputFlattened,
       outputTensors,
+      ncclOpts.cudaStreams,
       [&](at::Tensor& input,
           at::Tensor& output,
           ncclComm_t comm,
