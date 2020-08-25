e89290c6fcad3d8c2461cc06c4f4f024064e51e2
Michael Suo <suo@fb.com>
2020-08-25 11:38:07 -0700

    [jit] Better match behavior of loaded ScriptModules vs. freshly created ones
    IR emitter uses `ModuleValue` to represent ScriptModules and emit IR for
    attribute access, submodule access, etc.
    
    `ModuleValue` relies on two pieces of information, the JIT type of the
    module, and the `ConcreteModuleType`, which encapsulates Python-only
    information about the module.
    
    ScriptModules loaded from a package used to create a dummy
    ConcreteModuleType without any info in it. This led to divergences in
    behavior during compilation.
    
    This PR makes the two ways of constructing a ConcreteModuleType equivalent,
    modulo any py-only information (which, by definition, is never present in
    packaged files anyway).
    
    ghstack-source-id: 6baa83f0ac2e5b546ef316860ec4b710614e3e08
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43298

--- test/test_jit.py
+++ test/test_jit.py
@@ -15182,6 +15182,42 @@ a")
         with self.assertRaisesRegex(RuntimeError, 'has no attribute'):
             torch.jit.script(wrapped)
 
+    def test_rescripting_loaded_modules(self):
+        class InnerSubmod(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.register_buffer("foo", torch.ones(1))
+                self.register_parameter("bar", torch.nn.Parameter(torch.ones(1)))
+                self.baz = torch.ones(1)
+
+            def forward(self, x):
+                return x + x
+
+        class Inner(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.submod = InnerSubmod()
+
+            def forward(self, x):
+                return self.submod(x)
+
+        class Wrapper(nn.Module):
+            def __init__(self, inner):
+                super().__init__()
+                self.inner = inner
+
+            def forward(self, x):
+                # access inner elements
+                return self.inner.submod(x) + self.inner.submod.foo + self.inner.submod.bar + self.inner.submod.baz
+
+        inner_module = torch.jit.script(Inner())
+        wrapped = Wrapper(inner_module)
+        self.checkModule(wrapped, torch.ones(1))
+
+        inner_module_loaded = self.getExportImportCopy(inner_module)
+        wrapped_loaded = Wrapper(inner_module_loaded)
+        self.assertEqual(wrapped(torch.ones(1)), wrapped_loaded(torch.ones(1)))
+
 
 # known to be failing in tracer
 EXCLUDE_TRACED = {
